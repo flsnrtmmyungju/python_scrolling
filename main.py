d097c94a28472293ff4222127cbab3f905bbae5b
flsnrtmmyungju <47617495+flsnrtmmyungju@users.noreply.github.com>
2022-01-19 13:26:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+os.system("clear")
+
+#-----알바몬 슈퍼브랜드 이름,링크
+alba = requests.get("http://www.alba.co.kr")
+alba = BeautifulSoup(alba.text,"html.parser")
+
+companyList={}
+for a in alba.select('#MainSuperBrand > ul > li > a.goodsBox-info'):
+    title_data = ''
+    span = a.select('.company')
+    for title in span:  
+      title_data = title.text   
+    companyList[a] = {'title' :title_data,
+                    'companyList':a["href"]}
+                
+
+
+AlIST = list(companyList.values())
+
+for company in AlIST :
+  print(company['title'])
+  file=open(f'{company["title"]}.csv'.replace("/","_"), mode='w')
+  writer = csv.writer(file)
+  #writer.writerow(["place","title","time","pay","date"])
+
+
+
+
+  # save_to_file(company)
+
+
+
+
+
+
+
+
+
+
+
+
